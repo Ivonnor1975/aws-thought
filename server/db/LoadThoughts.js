0499d28f556a1e86fc3f071dec5610424da7fbdc
Ivon <ivonne@jjf.com>
2022-02-23 10:20:26 -0600

    add API

---
+++ server/db/LoadThoughts.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk");
+const fs = require('fs');
+
+//modify the AWS config object that DynamoDB will use to connect to the local instance
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "http://localhost:8000"
+  });
+
+  // create the DynamoDB service object
+const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+
+//use fs package to read users.json and assign the the object to const allusers
+console.log("Importing thoughts into DynamoDB. Please wait.");
+const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
+
+//loop over all users array and create the params objects
+allUsers.forEach(user => {
+    const params = {
+      TableName: "Thoughts",
+      Item: {
+        "username": user.username,
+        "createdAt": user.createdAt,
+        "thought": user.thought
+      }
+    };
+    //make a call to db with pub method.
+    dynamodb.put(params, (err, data) => {
+        if (err) {
+          console.error("Unable to add thought", user.username, ". Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("PutItem succeeded:", user.username);
+        }
+    });
+});
---
+++ server/routes/user-routes.js
@@ -0,0 +1,82 @@
+//use express and router() to create routes
+const express = require('express');
+const router = express.Router();
+
+//Configure the service interface
+const AWS = require("aws-sdk");
+const awsConfig = {
+  region: "us-east-1",
+  endpoint: "http://localhost:8000",
+
+};
+AWS.config.update(awsConfig);
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const table = "Thoughts";
+
+//create the get route to access all thoughts
+router.get('/users', (req, res) => {
+  const params = {
+    TableName: table
+  };
+  // Scan return all items in the table
+  dynamodb.scan(params, (err, data) => {
+    if (err) {
+      res.status(500).json(err); // an error occurred
+    }else {
+      res.json(data.Items)
+    }
+  });
+})
+
+//Create the GET Route to Access All Thoughts from a User
+router.get('/users/:username', (req, res) => {
+  console.log(`Querying for thought(s) from ${req.params.username}.`);
+  //declare params to define the query call to DynamoDB. We'll use the username retrieved from req.params to provide a condition for the query
+  const params = {
+    TableName: table,
+    KeyConditionExpression: "#un = :user",
+    ExpressionAttributeNames: {
+      "#un": "username",
+      "#ca": "createdAt",
+      "#th": "thought"
+    },
+    ExpressionAttributeValues: {
+      ":user": req.params.username
+    },
+    ProjectionExpression: "#th, #ca",
+    ScanIndexForward: false
+  };
+  //use the service interface object, dynamodb, and the query method to retrieve the user's thoughts from the database
+  dynamodb.query(params, (err, data) => {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log("Query succeeded.");
+      res.json(data.Items)
+    }
+  });
+});
+
+//Create the POST Route to Create a New Thought
+router.post('/users', (req, res) => {
+  const params = {
+    TableName: table,
+    Item: {
+      "username": req.body.username,
+      "createdAt": Date.now(),
+      "thought": req.body.thought
+    }
+  };
+  dynamodb.put(params, (err, data) => {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      res.status(500).json(err); // an error occurred
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+      res.json({"Added": JSON.stringify(data, null, 2)});
+    }
+  });
+});  // ends the route for router.post('/users')
+
+module.exports = router;
